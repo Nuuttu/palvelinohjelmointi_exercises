108555fc7e4071aa26579810b764c20bef2bea75
unknown <miettinen1tuomo@gmail.com>
2021-09-27 14:56:51 +0300

    book users

--- bookstore/src/main/java/com/example/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -12,6 +12,9 @@ import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
 import com.example.bookstore.domain.Category;
 import com.example.bookstore.domain.CategoryRepository;
+import com.example.bookstore.domain.User;
+import com.example.bookstore.domain.UserRepository;
+
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -22,7 +25,7 @@ public class BookstoreApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository bookrepo, CategoryRepository catrepo) {
+	public CommandLineRunner bookDemo(BookRepository bookrepo, CategoryRepository catrepo, UserRepository urepository) {
 		return (args) -> {
 			log.info("save a couple of categories and books");
 			catrepo.save(new Category("Thriller"));
@@ -32,6 +35,11 @@ public class BookstoreApplication {
 			bookrepo.save(new Book("Book2", "Kateson", 1992, 29299292, 20.2, catrepo.findByName("Thriller").get(0)));
 			bookrepo.save(new Book("Book1", "Johnson", 1990, 1293129393, 20.1, catrepo.findByName("Scifi").get(0)));
 			
+      			// Create users: admin/admin user/user
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
 			
 			log.info("fetch all books");
 			for (Book book: bookrepo.findAll()) {
--- bookstore/src/main/java/com/example/bookstore/web/BookController.java
+++ bookstore/src/main/java/com/example/bookstore/web/BookController.java
@@ -1,101 +1,94 @@
 package com.example.bookstore.web;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
 import com.example.bookstore.domain.CategoryRepository;
 
-
 @Controller
 public class BookController {
-	
-	@Autowired
-	private CategoryRepository catrepo;
-	
-	@Autowired
-	private BookRepository repository;
-	
-    @RequestMapping(value= {"/", "/bookstore"})
-    public String books(Model model) {
-        model.addAttribute("books", repository.findAll());
-        return "bookstore";
-    }
-    
-    @RequestMapping(value="/books", method = RequestMethod.GET)
-    public @ResponseBody List<Book> bookListRest() {
-    	return (List<Book>) repository.findAll();
-    }
-    
-    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
-    public @ResponseBody Optional<Book> FindBookRest(@PathVariable("id") Long bookId) {
-    	return repository.findById(bookId);
-    }
-  
-   
-    
-    @RequestMapping(value = "/add")
-    public String addBook(Model model){
-    	model.addAttribute("book", new Book());
-    	model.addAttribute("categories", catrepo.findAll());
-        return "addbook";
-    }
-    
-    @RequestMapping(value = {"/save"}, method = RequestMethod.POST)
-    public String save(Book book){
-	    repository.save(book);
-        return "redirect:bookstore";
-    }
-  
-	
-    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
-    public String editBook(@PathVariable("id") Long bookId, Model model){
-    	model.addAttribute("book", repository.findById(bookId));
-    	model.addAttribute("categories", catrepo.findAll());
-        return "editbook";
-    }
-    
-    @RequestMapping(value = "/edit/save", method = RequestMethod.POST)
-    public String edit(Book book){
-    	/*
-	    if (book.getId() != null ) {
-	    	
-	    	Book editedBook = repository.findById(book.getId()).orElse(null);
-	    	
-	    	editedBook.setId(book.getId());
-	    	editedBook.setTitle(book.getTitle());
-	    	editedBook.setAuthor(book.getAuthor());
-	    	editedBook.setYear(book.getYear());
-	    	editedBook.setIsbn(book.getIsbn());
-	    	editedBook.setPrice(book.getPrice());
-			repository.save(editedBook);
-			
-	    }
-	    */
-	    repository.save(book);
-        return "redirect:../bookstore";
-    }
-       
 
-    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
-    	repository.deleteById(bookId);
-        return "redirect:../bookstore";
-    }
+  @Autowired
+  private CategoryRepository catrepo;
 
-    
+  @Autowired
+  private BookRepository repository;
+
+  @RequestMapping(value="/login")
+  public String login() {	
+      return "login";
+  }	
+
+  @RequestMapping(value = { "/", "/bookstore" })
+  public String books(Model model) {
+    model.addAttribute("books", repository.findAll());
+    return "bookstore";
+  }
+
+  @RequestMapping(value = "/books", method = RequestMethod.GET)
+  public @ResponseBody List<Book> bookListRest() {
+    return (List<Book>) repository.findAll();
+  }
+
+  @RequestMapping(value = "/book/{id}", method = RequestMethod.GET)
+  public @ResponseBody Optional<Book> FindBookRest(@PathVariable("id") Long bookId) {
+    return repository.findById(bookId);
+  }
+
+  @RequestMapping(value = "/add")
+  public String addBook(Model model) {
+    model.addAttribute("book", new Book());
+    model.addAttribute("categories", catrepo.findAll());
+    return "addbook";
+  }
+
+  @RequestMapping(value = { "/save" }, method = RequestMethod.POST)
+  public String save(Book book) {
+    repository.save(book);
+    return "redirect:bookstore";
+  }
+
+  @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+  public String editBook(@PathVariable("id") Long bookId, Model model) {
+    model.addAttribute("book", repository.findById(bookId));
+    model.addAttribute("categories", catrepo.findAll());
+    return "editbook";
+  }
+
+  @RequestMapping(value = "/edit/save", method = RequestMethod.POST)
+  public String edit(Book book) {
+    /*
+     * if (book.getId() != null ) {
+     * 
+     * Book editedBook = repository.findById(book.getId()).orElse(null);
+     * 
+     * editedBook.setId(book.getId()); editedBook.setTitle(book.getTitle());
+     * editedBook.setAuthor(book.getAuthor()); editedBook.setYear(book.getYear());
+     * editedBook.setIsbn(book.getIsbn()); editedBook.setPrice(book.getPrice());
+     * repository.save(editedBook);
+     * 
+     * }
+     */
+    repository.save(book);
+    return "redirect:../bookstore";
+  }
+
+  @PreAuthorize("hasAuthority('ADMIN')")
+  @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+  public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+    repository.deleteById(bookId);
+    return "redirect:../bookstore";
+  }
+
 }
